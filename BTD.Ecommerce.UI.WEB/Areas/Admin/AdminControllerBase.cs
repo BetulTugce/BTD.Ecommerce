0e004d34f29d989659ab81cdaeaf30a37240e445
Betül Tuğçe Dikdoğmuş <79140478+BetulTugce@users.noreply.github.com>
2021-05-29 11:26:48 +0300

    website added

---
+++ BTD.Ecommerce.UI.WEB/Areas/Admin/AdminControllerBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BTD.Ecommerce.UI.WEB.Areas.Admin
+{
+    public class AdminControllerBase : Controller
+    {
+        protected override void Initialize(RequestContext requestContext)
+        {
+            var IsLogin = false;
+            if (requestContext.HttpContext.Session["AdminLoginUser"] == null)
+            {
+                //Admin girişi yapılmamış
+                requestContext.HttpContext.Response.Redirect("Admin/AdminLogin"); //MVC devreye girmedi ezdiğimiz için aşağıdakini yazamıyoruz. Mvcyi ezdik.
+                //Redirect("Admin/AdminLogin");
+            }
+            else
+            {
+                //Sorun yok admin içeride, sayfayı çalıştır.
+            }
+            base.Initialize(requestContext);
+        }
+    }
+}
---
+++ BTD.Ecommerce.UI.WEB/Areas/Admin/Controllers/AdminLoginController.cs
@@ -0,0 +1,36 @@
+﻿using BTD.Ecommerce.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTD.Ecommerce.UI.WEB.Areas.Admin.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        // GET: Admin/AdminLogin
+        BTDDB db = new BTDDB();
+        public ActionResult Index()
+        {
+            //Buraya view eklerken masterpagee dahil etmicem. Yani layout kullanmicam. Çünkü masterpageimi görmemesi lazım.
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Email, string Password)
+        {
+            var data = db.Users.Where(x => x.Email == Email && x.Password == Password && x.IsActive == true && x.IsAdmin == true).ToList();
+            if (data.Count==1)
+            {
+                //Doğru giriş
+                Session["AdminLoginUser"] = data.FirstOrDefault(); //Tüm tabloyu, tüm verileri içine attım
+                return Redirect("/admin");
+            }
+            else
+            {
+                //Hatalı giriş. Aynı sayfaya dönecek
+                return View();
+            }           
+        }
+    }
+}
